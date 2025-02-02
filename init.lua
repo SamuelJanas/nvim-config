5632da8c9ebc87c315c7c1dfbdbe76dfb6c4e08d
Samuel Janas <sneakyinsect@gmail.com>
2025-02-02 18:46:30 +0100

    Noeformat and lualine

--- init.lua
+++ init.lua
@@ -6,13 +6,15 @@ vim.cmd("set relativenumber")
 vim.cmd("set scrolloff=12")
 vim.cmd("set shiftwidth=4 smarttab")
 vim.cmd("set expandtab")
+vim.cmd("set laststatus=3")
 
 vim.g.lazyvim_cmp = "auto"
 
 local isLspDiagnosticsVisible = true
 vim.keymap.set("n", "<leader>lx", function()
-    isLspDiagnosticsVisible = not isLspDiagnosticsVisible
-    vim.diagnostic.config({
-        virtual_text = isLspDiagnosticsVisible,
-        underline = isLspDiagnosticsVisible
-    }) end)
+	isLspDiagnosticsVisible = not isLspDiagnosticsVisible
+	vim.diagnostic.config({
+		virtual_text = isLspDiagnosticsVisible,
+		underline = isLspDiagnosticsVisible,
+	})
+end)
