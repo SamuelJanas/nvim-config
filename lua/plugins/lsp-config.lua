384f061294c95ed96356996f5c9c6758745e5c78
Samuel Janas <sneakyinsect@gmail.com>
2025-02-05 09:22:40 +0100

    no typechecking

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,119 +1,120 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      
-      -- Setup nvim-cmp
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-      
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'buffer' },
-          { name = 'path' },
-        }),
-      })
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- LSP keymaps
-      local on_attach = function(client, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        
-        -- Go to definition
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        -- Hover information
-        vim.keymap.set('n', 'gH', vim.lsp.buf.hover, opts)
-        
-        -- Additional useful keymaps (optional)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>', { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-      end
+			-- Setup nvim-cmp
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 
-      -- Setup language servers
-      local lspconfig = require('lspconfig')
-      
-        lspconfig.yamlls.setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-        })
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
 
-        lspconfig.gopls.setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-        })
-      
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          python = {
-            analysis = {
-              typeCheckingMode = "basic",
-                autoSearchPaths = true,
-            }
-          }
-        }
-      })
-      
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "pyright",
-          "yamlls",
-          "gopls",
-        },
-        automatic_installation = true,
-      })
-    end,
-  },
-    {
-      "ray-x/lsp_signature.nvim",
-      event = "VeryLazy",
-      opts = {},
-      config = function(_, opts) require'lsp_signature'.setup(opts) end
-    },
+			-- LSP keymaps
+			local on_attach = function(client, bufnr)
+				local opts = { noremap = true, silent = true, buffer = bufnr }
+
+				-- Go to definition
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				-- Hover information
+				vim.keymap.set("n", "gH", vim.lsp.buf.hover, opts)
+
+				-- Additional useful keymaps (optional)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", { noremap = true, silent = true })
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+			end
+
+			-- Setup language servers
+			local lspconfig = require("lspconfig")
+
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					python = {
+						analysis = {
+							typeCheckingMode = "off",
+							autoSearchPaths = true,
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"pyright",
+					"yamlls",
+					"gopls",
+				},
+				automatic_installation = true,
+			})
+		end,
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {},
+		config = function(_, opts)
+			require("lsp_signature").setup(opts)
+		end,
+	},
 }
