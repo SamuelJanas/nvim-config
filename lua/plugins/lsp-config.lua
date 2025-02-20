036b6a32f8610dee9305db1e5fa0440442d196a1
Samuel Janas <sneakyinsect@gmail.com>
2025-02-20 16:37:02 +0100

    changed autocomplete

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -89,27 +89,26 @@ return {
 			},
 		})
 
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- For luasnip users.
-			}, {
-				{ name = "buffer" },
-			}),
-		})
+		--
+		-- cmp.setup({
+		-- 	snippet = {
+		-- 		expand = function(args)
+		-- 			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+		-- 		end,
+		-- 	},
+		-- 	mapping = cmp.mapping.preset.insert({
+		-- 		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+		-- 		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+		-- 		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		-- 		["<C-Space>"] = cmp.mapping.complete(),
+		-- 	}),
+		-- 	sources = cmp.config.sources({
+		-- 		{ name = "nvim_lsp" },
+		-- 		{ name = "luasnip" }, -- For luasnip users.
+		-- 	}, {
+		-- 		{ name = "buffer" },
+		-- 	}),
+		-- })
 		local opts = { noremap = true, silent = true, buffer = bufnr }
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 		-- Hover information
@@ -118,7 +117,7 @@ return {
 		-- Additional useful keymaps (optional)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 		-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 		vim.diagnostic.config({
 			-- update_in_insert = true,
